799246f05bbfea79ec0857f9a097fcbc32c5523a
vinagoros <98480269+vinagoros@users.noreply.github.com>
2022-10-02 00:35:01 +0100

    finished first draft of work on module 2

---
+++ ssg/parsers.py
@@ -0,0 +1,34 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+
+class Parser:
+    def __init__(self):
+        self.extensions = list[str]
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path) as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = self.dest / path.with_suffix().name
+        with open(full_path) as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    def __init__(self):
+        self.extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        super(ResourceParser,self).copy(path, source, dest)

39285e2fcc5c73b164fa8b1aa6b3bad4a8c145ac
vinagoros <98480269+vinagoros@users.noreply.github.com>
2022-10-02 02:43:20 +0100

    added 4 to 11 step to module 3~

--- ssg/content.py
+++ ssg/content.py
@@ -7,13 +7,35 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
+    @classmethod
     def load(cls,string):
         _, fm, content = cls.__regex.split(string,2)
-        load(fm,Loader=FullLoader)
+        metadata = load(fm,Loader=FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
-        self.data.update(("content",content))
+        self.data["content"] = content
 
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        if "type" in self.data.keys():
+            return self.data["type"]
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self,key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
     

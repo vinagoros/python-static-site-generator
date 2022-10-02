0a87ef5d0c1ca2ce0e768513f32112aa81b1b70a
vinagoros <98480269+vinagoros@users.noreply.github.com>
2022-10-02 01:07:10 +0100

    step 2 of module 3

--- ssg/content.py
+++ ssg/content.py
@@ -3,9 +3,9 @@ import re
 from collections.abc import Mapping
 
 class Content(Mapping):
-    def __init__(self):
-        self.__delimiter = "^(?:-|\+){3}\s*$"
-        self.__regex = re.compile(self.__delimiter, re.MULTILINE)
+
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     def load(self,cls,string):
         split_regex = self.__regex.split(string,2)

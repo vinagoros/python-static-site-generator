37d4080537590b0e0b3e90083a77e5ecf96dcfb1
vinagoros <98480269+vinagoros@users.noreply.github.com>
2022-10-02 03:16:56 +0100

    added fix to module 3

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,7 +42,7 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path,dest,html)
-        sys.stdout.write(f"\x1b[1;32m{path.name} converted to HTML. Metadata: {content}\n")
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
 
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
@@ -50,4 +50,4 @@ class ReStructuredTextParser(Parser):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
         self.write(path,dest,html["html_body"])
-        sys.stdout.write(f"\x1b[1;32m{path.name} converted to HTML. Metadata: {content}\n")
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
